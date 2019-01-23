8bdc40616a723c68edf965e9020f5845d7455058
dan <josh@deltstl.com>
2019-01-23 16:51:23 -0600

    initial commit

---
+++ web/app/themes/timberrunbuilders/page-templates/home.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* Template Name: HOME
+* Description: Timber Run Homepage
+*
+* @package WordPress
+* @subpackage BootstrapWP
+*/
+get_header();?>
+  <div class="tree-container tree-container--homepage tree-container--mobile centered-relative">
+    <div class="tree-container__fb hexagon">
+      <a class="hexagon__supporting-text" href="#">
+        <h2>Check us out on Facebook<span class="right-arrow"></span></h2>
+      </a>
+    </div>
+    <div class="centered-absolute tree-container__supporting-text">
+      <h1 class="">Timber Run Builders</h1>
+      <h3 class="">If you can dream it, we can build it.</h3>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <section id="philosophy" class="row services">
+      <h2>Our Philosophy</h2>
+      <p>At Timber Run Builders, our philosophy is simple. We put the utmost amount of care and craftsmanship into every task. We understand that your needs should be of the highest concern when completing a project and we take special care to ensure that
+        our customers are satisfied with our work.</p>
+    </section>
+    <section id="our-motto" class="row">
+      <h2>We've helped over 100 clients realize their dreams.</h2>
+      <p>Our motto is something we adhere to in everything we do. <span class="quote">If you can dream it, we can build it!</span> and it's true. There has never been a project that we've not been able to complete start to finish. You can rest assured that
+        your ideas are in good hands with us.</p>
+      <div class="hexagon our-motto__supporting-image">
+        <h2>Satisfaction Guarunteed!</h2>
+      </div>
+    </section>
+    <section id="homepage-services" class="row">
+      <div class="col-sm-12">
+        <h2>Services</h2>
+        <hr>
+      </div>
+      <div class="col-12 col-sm-4">
+        <h3>Room Additions</h3>
+        <p>
+          Adding a new room to your house not only increases your available living space, but also your investment value. Whether it’s a new bedroom, den, or garage, our team works closely with you to get the house you want.
+        </p>
+        <hr>
+      </div>
+      <div class="col-12 col-sm-4">
+        <h3>Kitchen Remodeling</h3>
+        <p>
+          Tired of the 1950’s kitchen cabinets and 1970’s appliances? From new breakfast rooms to appliances to new cabinetry, our kitchen remodeling team will design the kitchen of your dreams. Let us show you how top quality finishes and the latest in functional
+          design techniques can spice up your kitchen/dining experience.
+        </p>
+        <hr>
+      </div>
+      <div class="col-12 col-sm-4">
+        <h3>Bathroom Remodeling</h3>
+        <p>
+          Quality workmanship shines thru on our bathroom remodeling. We combine luxury finishes, basins, cabinetry and appliances from all major brands. We can install new materials and work with your existing tile or sinks. We only use experienced master plumbers
+          and electricians to build the comfort and elegance you desire.
+        </p>
+        <hr>
+      </div>
+      <div class="col-sm-12">
+        <a href="/services.html">Read More</a>
+      </div>
+    </section>
+    <section id="gallery" class="row">
+      <div class="col-sm-6 section__supporting-text">
+        <h2>Gallery</h2>
+        <p>Seeing is believing! Visit our gallery below to witness some of our quality craftsmanship.</p>
+        <a class="btn">Visit our Gallery</a>
+      </div>
+      <div class="centered-relative col-sm-6">
+        <img src="img/bathroom.jpg">
+      </div>
+    </section>
+  </div>
+<?php get_footer(); ?>
